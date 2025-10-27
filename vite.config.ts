97a5002487063db34f17e5bd911374a2fcf34b86
Khethelo Nkosi <saneleknkosi@gmail.com>
2025-10-27 16:20:38 +0200

    update(deploy): Tried fixing the yml file and the package.json file

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { fileURLToPath } from "url";
 
 // https://vitejs.dev/config/
 // export default defineConfig(({ mode }) => ({
@@ -24,4 +25,9 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig({
   plugins: [react()],
   base: '/immortal-cinematic-studio/',
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 })
